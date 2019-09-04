81c844ea264c3141736d119ea878ee3662638dfd
chenerous <chenerous@gmail.com>
2019-09-03 22:36:27 -0400

    Add shiny app implementation of leaflet plotting

---
+++ spi_shiny_app/app.R
@@ -0,0 +1,114 @@
+####  Shiny app implementation of plotSPI_leaflet.R
+####
+####  create map of SPI using leaflet 
+####
+#### inputs:  
+####  output of the SPI code
+####  a US county shapefile 
+####
+#### shapefile from:
+#### https://catalog.data.gov/dataset/tiger-line-shapefile-2016-nation-u-s-current-county-and-equivalent-national-shapefile/resource/fa774c9d-a098-4792-bfd4-94c7caa190b6
+####
+#### libraries required: leaflet, sf
+####
+#### 
+
+library(shiny)
+library(leaflet)
+library(sf)
+
+###########################################################################
+#### all the reading of data and processing that only needs to be done once
+###########################################################################
+
+#### read SPI data
+load("data/spi/SPI_out_3month.RData")
+
+### pick out which estimate you want to use z, z_emp, z_fit
+zplot <- spi_results$z
+Lon = spi_results$Lon
+Lat = spi_results$Lat
+
+### read shapefile, project to WGS84
+counties <- st_read("data/us_counties/tl_2016_us_county.shp",stringsAsFactors=FALSE)
+counties <- st_transform(counties,crs ="+proj=longlat +datum=WGS84")
+
+#### pull out only contiguous US
+contig_counties <- subset(counties,!((counties$STATEFP) %in% c("02","03","07","14","15","43","52","60","66","69","72","78")))
+
+#### create dataset of county values for every month in spi time series, then only pick correct data set when rendering (below)
+
+#### color counties by closest cell value of z
+#### calculating a county average would be nice to add later
+#### might be faster way to do this (to avoid loop), but only 3108 counties
+contig_counties$SPI= 0
+for (icounty in 1:nrow(contig_counties)){
+  i=which(abs(as.numeric(contig_counties$INTPTLON[icounty])-Lon) == min(abs(as.numeric(contig_counties$INTPTLON[icounty])-Lon)))
+  j=which(abs(as.numeric(contig_counties$INTPTLAT[icounty])-Lat) == min(abs(as.numeric(contig_counties$INTPTLAT[icounty])-Lat)))
+  contig_counties$SPI[icounty]=zplot[i,j,iMonth]
+}
+
+
+###########################################################################
+#### ui
+###########################################################################
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+  sliderInput("monthrange", "Month", 1, 12,value = 6, step =1,ticks = FALSE,sep=""),
+  sliderInput("yearrange", "Year", 1989, 2018, value = 2000, step = 1,ticks = FALSE,sep="")
+)
+
+###########################################################################
+#### server function
+###########################################################################
+
+server <- function(input, output, session) {
+  
+  output$mymap <- renderLeaflet({
+    
+    ### pick out which month and year you want
+    month = input$monthrange[1]
+    year = input$yearrange[1]
+    
+    iMonth=which(spi_results$monthList == month & spi_results$yearList == year)
+    
+    #### color counties by closest cell value of z
+    #### calculating a county average would be nice to add later
+    #### might be faster way to do this (to avoid loop), but only 3108 counties
+    contig_counties$SPI= 0
+    for (icounty in 1:nrow(contig_counties)){
+      i=which(abs(as.numeric(contig_counties$INTPTLON[icounty])-Lon) == min(abs(as.numeric(contig_counties$INTPTLON[icounty])-Lon)))
+      j=which(abs(as.numeric(contig_counties$INTPTLAT[icounty])-Lat) == min(abs(as.numeric(contig_counties$INTPTLAT[icounty])-Lat)))
+      contig_counties$SPI[icounty]=zplot[i,j,iMonth]
+    }
+    
+    #### create leaflet
+    bins <- c(-3,-2,-1.5,-1,1,1.5,2,3)
+    pal <- colorBin("RdYlBu", domain = contig_counties$SPI, bins = bins)
+    
+    leaflet(contig_counties) %>%
+      addTiles() %>%
+      setView(-95, 38, zoom = 4) %>%
+      addPolygons(label = ~paste(NAMELSAD,": ",round(SPI,2),sep=""),
+                  fillColor = ~pal(SPI),
+                  stroke=FALSE,
+                  smoothFactor = 0,
+                  fillOpacity = 0.5,
+                  highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                      bringToFront = TRUE)) %>%
+      addLegend(pal = pal,
+                values = ~SPI,
+                opacity = 0.7,
+                title =  paste(as.character(month),"/",as.character(year),' SPI',sep=""),
+                position = "bottomright") 
+    
+    #### above map has stroke=FALSE  => no lines between counties
+    #### to put those lines back in, delete stroke=FALSE and add
+    ####  color = "#444444", weight = 1, 
+    
+  })
+}
+
+shinyApp(ui, server)
